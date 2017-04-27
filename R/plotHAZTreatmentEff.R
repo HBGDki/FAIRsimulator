b1e9a6b865e70c90aa3ec95a725bc92a2c8e91e5
Niclas Jonsson <Niclas.Jonsson@pharmetheus.com>
2017-04-27 09:42:53 +0200

    Started to move the FAIRstudy functions into the package.

--- R/plotHAZTreatmentEff.R
+++ R/plotHAZTreatmentEff.R
@@ -2,6 +2,10 @@
 #' 
 #' @description Plots the simulated data in a FAIRsimulator \code{study} object with treatment effects.
 #' @param StudyObj A FAIRsimulator \code{study} object. 
+#' @param treatmentNames A vector of treatment names. Default is the treatment names assigned at study initialisation.
+#' @param treatmentColors A vector of colors for the treatment effects. Has to be the same length as the \code{treatmentNames}.
+#' @param SoCtrt A vector of indicies for the \code{treatmentNames} indicating Standard of Care treatment arms. The default is set by grepping \code{treatmentNames} for the string 'SoC'.
+#' @param SoCcol A vector of color names to assign to the SoC treatment arms.
 #' @details Will produce a graph of individual change from cohort baseline HAZ. Treatment effects are visualised by linear regression lines
 #' for each treatment group.
 #' @return A \code{ggplot} object
@@ -11,10 +15,19 @@
 #' \dontrun{
 #' plotHAZTreatmentEff(StudyObj)
 #' }
-plotHAZTreatmentEff <- function(StudyObj) {
+plotHAZTreatmentEff <- function(StudyObj,
+                                treatmentNames = unique(unlist(StudyObj$StudyDesignSettings$Treatments)),
+                                treatmentColors = ggplotColours(length(treatmentNames)),
+                                SoCtrt = grep(pattern = "SoC",unique(unlist(StudyObj$StudyDesignSettings$Treatments))),
+                                SoCcol = "black") {
 
+
+  if(length(treatmentNames) != length(treatmentColors)) stop("The number of treatment names myst be the same as the number of treatment colors.")
+  names(treatmentColors) <- treatmentNames
+  
+  treatmentColors[SoCtrt] <- SoCcol
+  
   allData <- getAllSubjectData(StudyObj,covariates=NULL) 
-  allData$Treatment <- factor(allData$Treatment,levels=sort(unique(allData$Treatment)),labels=paste("Treatment",sort(unique(allData$Treatment))))
   allData <- allData %>% group_by(StudyID,Cycle) %>% mutate(HAZcfb=HAZ-HAZ[1])
   
   p1 <- ggplot(allData,aes(CohortSampleTime/30,HAZcfb,group=StudyID)) +
@@ -25,7 +38,8 @@ plotHAZTreatmentEff <- function(StudyObj) {
     theme(legend.position="top") +
     xlab("Cohort time (months)") +
     ylab("HAZ - change from individual cohort baseline")+
-    facet_grid(Cycle~Cohort)
+    facet_grid(Cohort~Cycle) + 
+    scale_color_manual(values=treatmentColors)
   
   return(p1)
 }
