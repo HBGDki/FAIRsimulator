c35c9d6a9017ab16a7eb0145535ef3611d2a811b
Joakim Nyberg <joakim.nyberg@pharmetheus.com>
2017-04-28 13:02:22 +0200

    Added more default settings in create study and new method for adding new birth cohorts

--- FAIRStudyNJ.R
+++ FAIRStudyNJ.R
@@ -5,13 +5,21 @@ library(FAIRsimulator)
 #  # return(20) #20 randomized subjects per time unit
 # }
 
-StudyObj <- createStudy(nCohorts = 4,cohortStartTimes = c(2,1,0,6*30+20),samplingDesign = list(c(0,1,2,3,4,5,6)*30,c(0/30,3,6)*30,c(0/30,3,6)*30,c(0,1,2,3,4,5,6)*30),
-                        nSubjects = c(320,320,320,320),dropoutRates = c(0.2/(6*30),0.2/(6*30),0.2/(6*30),0.2/(6*30)), recruitmentAges = list(c(0,0.5)*30,c(6,6.5)*30,c(12,12.5)*30,c(0,0.5)*30))
+
+
+# StudyObj <- createStudy(nCohorts = 4,cohortStartTimes = c(2,1,0,6*30+20),samplingDesign = list(c(0,1,2,3,4,5,6)*30,c(0/30,3,6)*30,c(0/30,3,6)*30,c(0,1,2,3,4,5,6)*30),
+#                         nSubjects = c(320,320,320,320),dropoutRates = c(0.2/(6*30),0.2/(6*30),0.2/(6*30),0.2/(6*30)), recruitmentAges = list(c(0,0.5)*30,c(6,6.5)*30,c(12,12.5)*30,c(0,0.5)*30))
 
 createStudy <- function(nCohorts = 3, cohortStartTimes = c(2,1,0), samplingDesign = list(c(0,1,2,3,4,5,6)*30,c(0/30,3,6)*30,c(0/30,3,6)*30),nSubjects = c(320,320,320), 
-                        dropoutRates = c(0.2/(6*30),0.2/(6*30),0.2/(6*30)), 
+                        dropoutRates = c(0.2/(6*30),0.2/(6*30),0.2/(6*30)),randomizationProbabilities = list(c(0.25,0.25,0.25,0.25),c(0.25,0.25,0.25,0.25),c(0.25,0.25,0.25,0.25)),
+                        minAllocationProbabilities = list(c(0.25,0,0,0),c(0.25,0,0,0),c(0.25,0,0,0)),
+                        treatments =list(c("SoC-1","TRT-1","TRT-2","TRT-3"),c("SoC-2","TRT-4","TRT-5","TRT-6"),c("SoC-3","TRT-7","TRT-8","TRT-9")),
+                        effSizes = list(c(0,0.05,0.1,0.25),c(0,0,0.05,0.25),c(0,0.05,0.25,0.3)),
+                        newCohortLink = list(2,3,NULL),
                         recruitmentAges = list(c(0,1)*30,c(6,7)*30,c(12,13)*30),RecruitmentFunction=RecruitmentRatefunction,
-                        debugLevel=1) {
+                        debugLevel=1,studyStopTime=30*28,latestTimeForNewBirthCohorts=0,
+                        strCovariates=c("BIRTHWT","MAGE","MHTCM","SEXN","SANITATN"),
+                        currentDate = Sys.Date()) {
   
   ## Create the study design setting list ##
   
@@ -21,49 +29,39 @@ createStudy <- function(nCohorts = 3, cohortStartTimes = c(2,1,0), samplingDesig
   
   StudyDesignSettings$CohortStartTimes           <- cohortStartTimes
   
-  StudyDesignSettings$RandomizationProbabilities <- list(c(0.25,0.25,0.25,0.25), #Randomization probabilities for all possible treatments all cohorts
-                                                         c(0.25,0.25,0.25,0.25),
-                                                         c(0.25,0.25,0.25,0.25),
-                                                         c(0.25,0.25,0.25,0.25))
-  StudyDesignSettings$MinAllocationProbabilities <- list(c(0.25,0,0,0), #Minimum allocation probabilities for each treatment
-                                                         c(0.25,0,0,0),
-                                                         c(0.25,0,0,0),
-                                                         c(0.25,0,0,0))
-  
+  StudyDesignSettings$RandomizationProbabilities <- randomizationProbabilities #Initial randomization probabilities
+  StudyDesignSettings$MinAllocationProbabilities <- minAllocationProbabilities #Minimum allocation probabilities for each treatment
+                                                       
+
   StudyDesignSettings$iNumPosteriorSamples       <- 10000 #The number of samples to calculate prob of beeing best
   
-  StudyDesignSettings$Treatments <-list(
-    c("SoC-1","TRT-1","TRT-2","TRT-3"),
-    c("SoC-2","TRT-4","TRT-5","TRT-6"),
-    c("SoC-3","TRT-7","TRT-8","TRT-9"),
-    c("SoC-1","TRT-1","TRT-2","TRT-3")
-  ) 
+  StudyDesignSettings$Treatments <- treatments #The treatment
   
-  StudyDesignSettings$EffSizes <- list(
-    c(0,0.05,0.1,0.25),
-    c(0,0,0.05,0.25),
-    c(0,0.05,0.25,0.3),
-    c(0,0.05,0.1,0.25)
-  )
+  StudyDesignSettings$EffSizes <- effSizes #The actual effect sizes
   
   #The age ranges for each cohort
   StudyDesignSettings$CohortAgeRange <- recruitmentAges
   
   #The sampling design for each pre-defined cohort
-  #StudyDesignSettings$SamplingDesigns <- list(c(0,1,2,3,4,5,6)*30,c(0/30,3,6)*30,c(0/30,3,6,9,12)*30) 
-  
   StudyDesignSettings$SamplingDesigns <- samplingDesign
   
   #When a cohort is evolving to a new cohort, the information about randomization should be based on the cohort in the list, NULL= no evolving
-  StudyDesignSettings$NewCohortLink <- list(2,3,NULL,1) 
+  StudyDesignSettings$NewCohortLink <- newCohortLink 
   
   #If the last sample will be the baseline sample (subject cohorttime = 0) in the new cohort 
   StudyDesignSettings$MoveLastSampleToNewCohort <- TRUE 
   
+  #The dropout rates
   StudyDesignSettings$CohortDropoutRate <- dropoutRates
   
+  #The stop time of the study
+  StudyDesignSettings$StudyStopTime <- studyStopTime
+  
+  #The latest time to start new birth cohorts, set to 0 if no new birth cohorts should be added
+  StudyDesignSettings$LatestTimeForNewBirthCohorts<-latestTimeForNewBirthCohorts
+  
   #The covariates that should be stored on each subject, e.g. to be used in the lme analysis
-  StudyDesignSettings$Covariates <- c("BIRTHWT","MAGE","MHTCM","SEXN","SANITATN")  
+  StudyDesignSettings$Covariates <- strCovariates  
   
   #The current ID number, i.e. global counter of ID number
   StudyDesignSettings$CurrentID  <- 1 
@@ -72,7 +70,7 @@ createStudy <- function(nCohorts = 3, cohortStartTimes = c(2,1,0), samplingDesig
   StudyObj <- list()
   
   StudyObj$CurrentTime <- 0 #Let the study be time driven
-  StudyObj$CurrentDate <- Sys.Date()
+  StudyObj$CurrentDate <- currentDate #The date that the study will start
   
   ## Add recruitmentrate function
   StudyObj$Recruitmentfunction <- RecruitmentRatefunction
@@ -94,6 +92,7 @@ createStudy <- function(nCohorts = 3, cohortStartTimes = c(2,1,0), samplingDesig
   EventList[[length(EventList)+1]] <- MoveCompletedSubjects #Move completed subjects event
   EventList[[length(EventList)+1]] <- AnalyzeDataEvent #Add a Analyze data event
   EventList[[length(EventList)+1]] <- UpdateProbabilitiesEvent #Update all probabilities, even for non directly connected cohorts
+  EventList[[length(EventList)+1]] <- AddNewBirthCohortEvent #Add a new birth cohort
   
   StudyObj$EventList           <- EventList
   StudyObj$StudyDesignSettings <- StudyDesignSettings #Add the specific study design settings into the Study object
@@ -103,7 +102,7 @@ createStudy <- function(nCohorts = 3, cohortStartTimes = c(2,1,0), samplingDesig
   return(StudyObj)
 }
 
-#StudyObj <- createStudy(RecruitmentFunction = RecruitmentRatefunction)
+StudyObj <- createStudy(RecruitmentFunction = RecruitmentRatefunction)
 StudyObj<-AdaptiveStudy(StudyObj)
 
 plotStudyCohorts(StudyObj)
