1ebe2230eaf94058fcf4325e7ebdfeb29494603e
Niclas Jonsson <Niclas.Jonsson@pharmetheus.com>
2017-05-04 14:40:09 +0200

    Created functions to run multiple studies in parallel and plot the results.

--- Examples/example2.R
+++ Examples/example2.R
@@ -25,16 +25,93 @@ StudyObjIni <- createStudy(latestTimeForNewBirthCohorts=18*30,studyStopTime = 32
                         probTemperationFunction = probTemperation)
 
 
+iter   <- 28
+ncores <- 7
 
-registerDoParallel(cores=6)
+runMultiSim <- function(StudyOnjIni,extractProbs=TRUE,iter=1,ncores=1,...) {
+  
+  ## Start the parallell engine if ncores > 1
+  if(ncores>1) {registerDoParallel(cores=ncores)}
+  
+  ## Run the simulations
+  myRes <- foreach(i=1:iter) %dopar% AdaptiveStudy(StudyObjIni)
+  
+  ## Extract the probabilities if requested
+  if(extractProbs) {
+    probDf  <- getMultiProbList(myRes,ncores=ncores,...)
+    retList <- list(studList = myRes,probDf = probDf)
+  } else {
+    retList <- list(studList = myRes)
+  }
+  
+  return(retList)
+  
+}
 
-myRes <- foreach(i=1:12) %dopar% AdaptiveStudy(StudyObjIni)
+getMultiProbList <- function(multiStudObj,ncores=1,...) {
 
-tmp <- map(myRes,getProbData)
-bind_rows(tmp)
+  ## Start the parallell engine if ncores > 1
+  if(ncores>1) {registerDoParallel(cores=ncores)}
+  
+  ## get a list of all probability data
+  probList <- foreach(i=1:length(multiStudObj)) %dopar% getProbData(multiStudObj[[i]],...)
+  
+  ## Create one data frame of all the probability data frames
+  probDf <- bind_rows(probList)
+  
+  ## Add an iteration variable (Iter)
+  probDf$Iter <- rep(1:length(multiStudObj),each=nrow(probDf)/length(multiStudObj))
 
-## Extract the probabilities used in the plotProbs plot above.
+  return(probDf)  
+}
 
+myMultStud <- runMultiSim(StudyOnjIni,iter=iter,ncores=ncores)
 
+probDf           <- myMultStud[[2]]  # The Randomization probabilities
+probDfUnweighted <- getMultiProbList(myMultStud[[1]],ncores=ncores,strProb="UnWeightedRandomizationProbabilities")  # The UnweightedRandomization probabilities
 
-tmp <- getProbData(StudyObj)
+plotMultiProb(probDf)
+
+plotMultiProb(probDfUnweighted)
+
+plotMultiProb <- function(probDf,ylb="Randomization probability",pup = 0.95,pdo=0.05) {
+
+  ## Define functions to compute the upper and lower limits of the prediction intervals of the probabilities
+  up95 <- function(x,PRup=pup) quantile(x,p=PRup)
+  do05 <- function(x,PRdo=pdo) quantile(x,p=PRdo)
+
+  ## Compute the plot data
+  sumProbData <- probDf %>% 
+    group_by(CohortAge,TreatmentName,RandStudyTime) %>% 
+    summarise(Mean=mean(Prob),Up=up95(Prob),Down=do05(Prob)) 
+
+  ## To get legends for each age cohort we need to create separate graphs for the. We'll split the data and create the graphs and then print them 
+  ## with grid.arrange.
+  
+  xs <- split(sumProbData,f = sumProbData$CohortAge)
+
+  plotList <- list()
+
+  plotList[[1]] <- ggplot(xs[[1]],aes(RandStudyTime/30,Mean,group=TreatmentName,color=TreatmentName)) +
+    geom_point() +
+    geom_line() +
+    geom_ribbon(aes(ymin=Down,ymax=Up,fill=TreatmentName),alpha=0.5) +
+    theme(legend.position="top") +
+    labs(color=NULL,fill=NULL) +
+    xlab("Study time (months)") +
+    ylab(ylb) +
+    facet_grid(CohortAge~TreatmentName)
+  
+  for(i in 2:length(xs)) {
+    plotList[[i]] <- plotList[[1]] %+% xs[[i]]
+  }
+  
+  retVal <- plotList 
+  
+  ## print the plots  
+  plotList$nrow <- length(xs)
+  do.call(grid.arrange,plotList)
+  
+  # Return the plot list without printing
+  return(invisible(retVal))
+}
