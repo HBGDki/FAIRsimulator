f45696ecf18047ce1f2cbc501a1d5748d48fd359
Niclas Jonsson <Niclas.Jonsson@pharmetheus.com>
2017-05-05 14:43:31 +0200

    Created example 2 with multi run functionality. Added three functions to FAIRstudy.R to support it.

--- R/FAIRStudy.R
+++ R/FAIRStudy.R
@@ -1297,3 +1297,141 @@ plotProbs <- function(StudyObj,...) {
 probTemperation <- function(probs) {
   return(probs)
 }
+
+#' runMultiSim
+#' 
+#' Simulate multiple instances of a study, optionally in a parallel fashion.
+#'
+#' @param StudyOnjIni An initial FAIRsimulator \code{study} object created by \code{createStudy}.
+#' @param extractProbs Whether to also extract randomization probabilities (in parallel).
+#' @param iter The number of studies to simulate.
+#' @param ncores The number of cores to use for parallel execution. 
+#' @param ... Other parameters. Typically \code{strProb} to specify "UnWeightedRandomizationProbabilities" instead of the default "RandomizationProbabilities".
+#'
+#' @return A list with a list if the simulated FAIRsimulator \code{study} objects and, optionally, a data.frame with the randomizaion probabilities.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' StudyObjIni <- createStudy(latestTimeForNewBirthCohorts=18*30,studyStopTime = 32*30,
+#' nSubjects = c(320,320,320),
+#' randomizationProbabilities = list(rep(0.20,5),rep(0.20,5),rep(0.20,5)),
+#' #minAllocationProbabilities = list(c(0,rep(0,4)),c(0,rep(0,4)),c(0,rep(0,4))),
+#' minAllocationProbabilities = list(c(0.2,rep(0,4)),c(0.2,rep(0,4)),c(0.2,rep(0,4))),
+#' treatments =list(c("SoC-1","TRT-1","TRT-2","TRT-3","TRT-4"),c("SoC-2","TRT-5","TRT-6","TRT-7","TRT-8"),c("SoC-3","TRT-9","TRT-10","TRT-11","TRT-12")),
+#' effSizes = list(c(0,0.05,0.1,0.15,0.25),c(0,0.05,0.1,0.15,0.25),c(0,0.05,0.1,0.15,0.25)),
+#' Recruitmentfunction=function(...) {return(5000)},
+#' minSubjects = 10)
+#'
+#' myMultStud <- runMultiSim(StudyOnjIni,iter=10,ncores=2)
+#' }
+runMultiSim <- function(StudyOnjIni,extractProbs=TRUE,iter=1,ncores=1,...) {
+  
+  ## Start the parallell engine if ncores > 1
+  if(ncores>1) {registerDoParallel(cores=ncores)}
+  
+  ## Run the simulations
+  myRes <- foreach(i=1:iter) %dopar% AdaptiveStudy(StudyObjIni)
+  
+  ## Extract the probabilities if requested
+  if(extractProbs) {
+    probDf  <- getMultiProbList(myRes,ncores=ncores,...)
+    retList <- list(studList = myRes,probDf = probDf)
+  } else {
+    retList <- list(studList = myRes)
+  }
+  
+  return(retList)
+  
+}
+
+#' getMultiProbList
+#' 
+#' Extracts the probability lists from a list of FAIRsimulator \code{study} objects. It will optionally be done in parallel.
+#'
+#' @param multiStudObj A list of FAIRsimulator \code{study} objects
+#' @param ncores The number of cores to use. 
+#' @param ... Arguments to \code{getProbData}, in particular \code{strProb}
+#'
+#' @return A data frame with probabilities. Includes a column with Iteration number.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' getMultiProbList(multiStudObj,ncores=2,strProb="UnWeightedRandomizationProbabilities") 
+#' }
+getMultiProbList <- function(multiStudObj,ncores=1,...) {
+  
+  ## Start the parallell engine if ncores > 1
+  if(ncores>1) {registerDoParallel(cores=ncores)}
+  
+  ## get a list of all probability data
+  probList <- foreach(i=1:length(multiStudObj)) %dopar% getProbData(multiStudObj[[i]],...)
+  
+  ## Create one data frame of all the probability data frames
+  probDf <- bind_rows(probList)
+  
+  ## Add an iteration variable (Iter)
+  probDf$Iter <- rep(1:length(multiStudObj),each=nrow(probDf)/length(multiStudObj))
+  
+  return(probDf)  
+}
+
+#' plotMultiProb
+#' 
+#' Plots the updated probabilities over study time, per cohort, with prediction interval.
+#'
+#' @param probDf A data.frame with probabilities for multiple FAIRsimulator \code{study} objects.
+#' @param ylb y-axis label for 
+#' @param pup Upper percentile for the prediction interval.
+#' @param pdo Lower percentile for the prediction interval.
+#'
+#' @return An invisible list of ggplot objects as long as the number of cohorts.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' plotMultiProb(probDf)
+#' plotMultiProb(probDfUnweighted,ylb="Unweighted randomization probabilities")
+#' }
+plotMultiProb <- function(probDf,ylb="Randomization probability",pup = 0.95,pdo=0.05) {
+  
+  ## Define functions to compute the upper and lower limits of the prediction intervals of the probabilities
+  up95 <- function(x,PRup=pup) quantile(x,p=PRup)
+  do05 <- function(x,PRdo=pdo) quantile(x,p=PRdo)
+  
+  ## Compute the plot data
+  sumProbData <- probDf %>% 
+    group_by(CohortAge,TreatmentName,RandStudyTime) %>% 
+    summarise(Mean=mean(Prob),Up=up95(Prob),Down=do05(Prob)) 
+  
+  ## To get legends for each age cohort we need to create separate graphs for the. We'll split the data and create the graphs and then print them 
+  ## with grid.arrange.
+  
+  xs <- split(sumProbData,f = sumProbData$CohortAge)
+  
+  plotList <- list()
+  
+  plotList[[1]] <- ggplot(xs[[1]],aes(RandStudyTime/30,Mean,group=TreatmentName,color=TreatmentName)) +
+    geom_point() +
+    geom_line() +
+    geom_ribbon(aes(ymin=Down,ymax=Up,fill=TreatmentName),alpha=0.5) +
+    theme(legend.position="top") +
+    labs(color=NULL,fill=NULL) +
+    xlab("Study time (months)") +
+    ylab(ylb) +
+    facet_grid(CohortAge~TreatmentName)
+  
+  for(i in 2:length(xs)) {
+    plotList[[i]] <- plotList[[1]] %+% xs[[i]]
+  }
+  
+  retVal <- plotList 
+  
+  ## print the plots  
+  plotList$nrow <- length(xs)
+  do.call(grid.arrange,plotList)
+  
+  # Return the plot list without printing
+  return(invisible(retVal))
+}
