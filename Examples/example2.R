726a5521407960ff54f2ceb09c36a41cd237cfd2
Niclas Jonsson <Niclas.Jonsson@pharmetheus.com>
2017-05-09 08:25:48 +0200

    Updated examaples and scenarios

--- Examples/example2.R
+++ Examples/example2.R
@@ -13,23 +13,49 @@ probTemperation <- function(probs) {
 StudyObjIni <- createStudy(latestTimeForNewBirthCohorts=18*30,studyStopTime = 32*30,
                         nSubjects = c(320,320,320),
                         randomizationProbabilities = list(rep(0.20,5),rep(0.20,5),rep(0.20,5)),
-                        #minAllocationProbabilities = list(c(0,rep(0,4)),c(0,rep(0,4)),c(0,rep(0,4))),
                         minAllocationProbabilities = list(c(0.2,rep(0,4)),c(0.2,rep(0,4)),c(0.2,rep(0,4))),
                         treatments =list(c("SoC-1","TRT-1","TRT-2","TRT-3","TRT-4"),c("SoC-2","TRT-5","TRT-6","TRT-7","TRT-8"),c("SoC-3","TRT-9","TRT-10","TRT-11","TRT-12")),
                         effSizes = list(c(0,0.05,0.1,0.15,0.25),c(0,0.05,0.1,0.15,0.25),c(0,0.05,0.1,0.15,0.25)),
                         Recruitmentfunction=function(...) {return(5000)},
-                        minSubjects = 10,
                         probTemperationFunction = probTemperation)
 
 
 iter   <- 7
 ncores <- 7
 
-system.time(myMultStud <- runMultiSim(StudyOnjIni,iter=iter,ncores=ncores))
+system.time(myMultStud <- runMultiSim(StudyObjIni,iter=iter,ncores=ncores))
 
 probDf           <- myMultStud[[2]]  # The Randomization probabilities
 probDfUnweighted <- getMultiProbList(myMultStud[[1]],ncores=ncores,strProb="UnWeightedRandomizationProbabilities")  # The UnweightedRandomization probabilities
+probDfUnweightedUpdate <- getMultiProbList(myMultStud[[1]],ncores=ncores,strProb="UpdateProbabilities")  # The UnweightedRandomization probabilities
 
 plotMultiProb(probDf)
 plotMultiProb(probDfUnweighted,ylb="Unweighted randomization probabilities")
+plotMultiProb(probDfUnweightedUpdate,ylb="Unweighted update probabilities")
 
+## Summary of the end of study probabilities
+sumData <- 
+  probDfUnweighted %>% 
+  group_by(CohortAge) %>% 
+  filter(RandStudyTime == max(RandStudyTime)) %>% 
+  group_by(TreatmentName,CohortAge) %>% 
+  summarise(Mean=mean(Prob),Low=quantile(Prob,p=0.025),High=quantile(Prob,p=0.975))
+
+xs <- split(sumData,f = sumData$CohortAge)
+plotList <- list()
+
+plotList[[1]] <- 
+  ggplot(xs[[1]],aes(TreatmentName,Mean,color=TreatmentName)) +
+  geom_point() +
+  geom_errorbar(aes(ymin=Low,ymax=High),width=0.2) +
+  facet_wrap(~CohortAge,scales = "free_x")
+
+for(i in 2:length(xs)) {
+  plotList[[i]] <- plotList[[1]] %+% xs[[i]]
+}
+
+## print the plots  
+plotList$nrow <- length(xs)
+do.call(grid.arrange,plotList)
+  
+  
--- Examples/example3a.R
+++ Examples/example3a.R
@@ -47,3 +47,43 @@ StudyObj$CohortList[[1]]$UpdateCoefficients
 StudyObj$CohortList[[2]]$UpdateCoefficients
 StudyObj$CohortList[[3]]$UpdateCoefficients
 
+getProbData(StudyObj,strProb = "UnWeightedUpdateProbabilities")
+
+## Running the study multiple times to get statistics
+iter   <- 20
+ncores <- 7
+
+system.time(myMultStud <- runMultiSim(StudyOnjIni,iter=iter,ncores=ncores))
+
+probDf           <- myMultStud[[2]]  # The Randomization probabilities
+probDfUnweighted <- getMultiProbList(myMultStud[[1]],ncores=ncores,strProb="UnWeightedUpdateProbabilities")  # The UnweightedRandomization probabilities
+
+plotMultiProb(probDf)
+plotMultiProb(probDfUnweighted,ylb="Unweighted randomization probabilities")
+
+## Summary of the end of study probabilities
+sumData <- 
+  probDfUnweighted %>% 
+  group_by(CohortAge) %>% 
+  filter(RandStudyTime == max(RandStudyTime)) %>% 
+  group_by(TreatmentName,CohortAge) %>% 
+  summarise(Mean=mean(Prob),Low=quantile(Prob,p=0.025),High=quantile(Prob,p=0.975))
+
+xs <- split(sumData,f = sumData$CohortAge)
+plotList <- list()
+
+plotList[[1]] <- 
+  ggplot(xs[[1]],aes(TreatmentName,Mean,color=TreatmentName)) +
+  geom_point() +
+  geom_errorbar(aes(ymin=Low,ymax=High),width=0.2) +
+  facet_wrap(~CohortAge,scales = "free_x")
+
+for(i in 2:length(xs)) {
+  plotList[[i]] <- plotList[[1]] %+% xs[[i]]
+}
+
+## print the plots  
+plotList$nrow <- length(xs)
+do.call(grid.arrange,plotList)
+
+
